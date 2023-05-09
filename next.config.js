fa2cfe9fffe36aa4ca6f04ff34d026b6c21d0692
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-09 12:09:27 +0300

    start 🚀

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    AUTHORIZATION_TOKEN: process.env.AUTHORIZATION_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+  },
+};
+
+module.exports = nextConfig;
