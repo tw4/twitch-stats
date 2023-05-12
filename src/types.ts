31478be2699f97fffdf7db830593ca67e354f944
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-12 16:15:42 +0300

    add: type Streams

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type Streams = {
+  id: string;
+  user_id: string;
+  user_login: string;
+  user_name: string;
+  game_id: string;
+  game_name: string;
+  type: string;
+  title: string;
+  viewer_count: number;
+  started_at: Date;
+  language: string;
+  thumbnail_url: string;
+  tag_ids: any[];
+  tags: string[];
+  is_mature: boolean;
+};
