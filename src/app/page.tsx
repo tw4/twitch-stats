0b936cb95d37ecf2418a3c38599c2aca5938f02a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-22 11:24:46 +0300

    update: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image';
+import { getTopClipsMonth, getTopClipsToDay, getTopClipsWeek } from '@/api';
+import ClipList from '@/components/clipsList/ClipList';
 
-export default function Home() {
+const Home = async () => {
+  const mostWatchedClipsToday = await getTopClipsToDay();
+  const mostWatchedClipsThisWeek = await getTopClipsWeek();
+  const mostWatchedClipsThisMonth = await getTopClipsMonth();
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="p-10 h-[94.3vh] w-[85vw] overflow-y-scroll scrollbar  dark:text-white">
+      {/* today's most popular clips side */}
+      <h1 className="font-bold text-3xl pt-5 pb-5">
+        TODAY'S MOST POPULAR CLIPS
+      </h1>
+      <ClipList clipList={mostWatchedClipsToday.data} />
+      {/* week's most popular clips side */}
+      <h1 className="font-bold text-3xl pt-5 pb-5">
+        WEEK'S MOST POPULAR CLIPS
+      </h1>
+      <ClipList clipList={mostWatchedClipsThisWeek.data} />
+      {/* month's most popular clips side */}
+      <h1 className="font-bold text-3xl pt-5 pb-5">
+        MONTH'S MOST POPULAR CLIPS
+      </h1>
+      <ClipList clipList={mostWatchedClipsThisMonth.data} />
     </main>
   );
-}
+};
+
+export default Home;
